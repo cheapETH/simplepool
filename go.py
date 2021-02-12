12bfffa86c9de882929c43e3099b53bdb96037d1
George Hotz <geohot@gmail.com>
2021-02-12 15:59:03 -0800

    fe

--- go.py
+++ go.py
@@ -22,11 +22,13 @@ def eth_submitWork(*params):
   print("***** SUBMIT WORK *****", params)
   return response.data.result
 
+xcnt = defaultdict(int)
 pcnt = defaultdict(int)
 import socketserver
 class MyTCPHandler(socketserver.StreamRequestHandler):
   def handle(self):
     cl = 0
+    addr = None
     while 1:
       hh = self.rfile.readline().strip()
       if b'POST /' in hh:
@@ -36,11 +38,16 @@ class MyTCPHandler(socketserver.StreamRequestHandler):
       if hh == b"":
         #print("done")
         break
+    if addr is None:
+      snd = "CONNECTED:"+str(xcnt)+"\n\n\nFOUND BLOCK:"+str(pcnt)
+      self.wfile.write(b'HTTP/1.0 200 OK\r\n\r\n'+snd.encode('utf-8'))
+      return
     idat = self.rfile.read(cl).decode('utf-8')
     print("<<", addr, idat)
     dat = dispatch(idat)
     print(">>", dat)
     print(pcnt)
+    xcnt[addr] += 1
     if 'eth_submitWork' in idat:
       pcnt[addr] += 1
     self.wfile.write(b'HTTP/1.0 200 OK\r\n\r\n'+str(dat).encode('utf-8'))
